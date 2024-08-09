3b3c5d70b2e5e9f1a8a4c86da90bafa58b219402
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-09 22:27:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let  name = prompt("Username kiriting")
+let  parol = prompt("Parol kiriting")
+        if (name == "Alfarruq" && parol == "farruq"){
+            alert("Xush kelibsiz")
+            console.log("Xush kelibsiz");
+        }else{
+            alert("Username yoki parol noto`g`ri")
+            console.log("Username yoki parol noto`g`ri");
+        }
